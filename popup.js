fdac321de2e18c01bd876ac37cac749def9b5172
Blueflier <joseph.e.hartono@gmail.com>
2021-03-31 22:55:08 -0700

    icon changer yessir

--- popup.js
+++ popup.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     
+    var checkPageButton = document.getElementById('checkPage');
+
+
+    checkPageButton.addEventListener('click', function() {
+
+        
     var URL;
     chrome.tabs.query({
         active:true,
@@ -7,13 +13,18 @@ document.addEventListener('DOMContentLoaded', function() {
     }, function(tabs){
         var tabURL = tabs[0].url;
         console.log(tabURL);
-        setURL(tabURL);
+        //setURL(tabURL);
         document.getElementById("checkPage").innerHTML = URL;
-});
+
+    chrome.browserAction.setIcon({path: {19:"images/ALogo-38.png"}});
+
+    });
+
+}, false);
 
     
    
-    function setURL(url){
-        URL = url;
-    };
+    // function setURL(url){
+    //     URL = url;
+    // };
 }, false);
