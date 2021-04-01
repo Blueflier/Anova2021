2466053d52043d027764b424c5608498f29c1932
hjkim015 <47065568+hjkim015@users.noreply.github.com>
2021-04-01 14:25:08 -0700

    Update url_parser.py
    Added an extension dictionary + score function

--- url_parser.py
+++ url_parser.py
@@ -1,11 +1,27 @@
 """
 Author: Hannah Kim
 Date: Tuesday, March 20th, 2021
-Purpose: Output the domain name of a website url
+Purpose: Returns the score of a website url based on the extension
 """
 from urllib.parse import urlparse
 
+url = 'https://www.npr.org/sections/news/'
+url_domain = urlparse(url).netloc
+print(domain)
 
-url = 'https://stackoverflow.com/questions/46889772/how-to-send-data-from-javascript-to-python'
-domain = urlparse(url).netloc
-print(domain) # --> www.example.test
+extension_dict = {
+    ".com": 5,
+    ".edu": 17,
+    ".gov": 77,
+    ".org": 3,
+    ".us": 4
+}
+
+def score(url_domain):
+    for x in extension_dict:
+        if x in url_domain:
+            # print(x)
+            # print("score is:", extension_dict[x])
+            return extension_dict[x]
+
+score(url_domain)
